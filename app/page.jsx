996ea356e6a17f4fa70b586b88503c97d8dc7295
Ahmed Lemssiah <art3que@gmail.com>
2024-10-14 16:29:13 +0200

    Components styling

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,6 @@
 import Link from "next/link";
+import Image from "next/image";
+import emma from "@/public/images/avatars/emma-richardson.jpg"
 const Home = ()  => {
   return (
     <>
@@ -7,6 +9,7 @@ const Home = ()  => {
           <h2 className="text__lg">Overview</h2>
         </div>
         {/* Content Box Default */}
+        <section className="section">
         <div className="content-box">
           <h3 className="content-box__title text__sm">Current Balance</h3>
           <div className="content-box__data text__lg text__bold">
@@ -15,8 +18,10 @@ const Home = ()  => {
             </p>
           </div>
         </div>
+        </section>
         {/* Content Box Default */}
         {/* Content Box dark */}
+        <section className="section">
         <div className="content-box content-box--dark">
           <h3 className="content-box__title text__sm">Current Balance</h3>
           <div className="content-box__data">
@@ -25,8 +30,10 @@ const Home = ()  => {
             </p>
           </div>
         </div>
+        </section>
         {/* Content Box dark */}
         {/* Content Box dark */}
+        <section className="section">
         <div className="content-box content-box--headfull">
           <div className="content-box__header">
               <h3 className="content-box__title text__md">Pots</h3>
@@ -57,18 +64,87 @@ const Home = ()  => {
               </div>
             </div>
           <div className="content-box--grid">
-              <div className="content-box content-box--data text-second-green" >
+            {/* Box with Line at the left side the classname text-... change the color of the line */}
+              <div className="content-box content-box--line-left text-second-green">
                   <h5 className="text__sm  text-dark-200">Savings</h5>
                   <div className="content-box__data text-dark-100">
                     <p className="text__sm text__bold">
-                      $4,836.00
+                      $159
                     </p>
                   </div>
               </div>
+            {/* Box with Line at the left side the classname text-... change the color of the line */}
+            {/* Box with Line at the left side the classname text-... change the color of the line */}
+              <div className="content-box content-box--line-left text-second-cyan">
+                  <h5 className="text__sm  text-dark-200">Gift</h5>
+                  <div className="content-box__data text-dark-100">
+                    <p className="text__sm text__bold">
+                      $40
+                    </p>
+                  </div>
+              </div>
+            {/* Box with Line at the left side the classname text-... change the color of the line */}
+            {/* Box with Line at the left side the classname text-... change the color of the line */}
+              <div className="content-box content-box--line-left text-third-magenta">
+                  <h5 className="text__sm  text-dark-200">Concert Ticket</h5>
+                  <div className="content-box__data text-dark-100">
+                    <p className="text__sm text__bold">
+                      $110
+                    </p>
+                  </div>
+              </div>
+            {/* Box with Line at the left side the classname text-... change the color of the line */}
+            {/* Box with Line at the left side the classname text-... change the color of the line */}
+              <div className="content-box content-box--line-left text-second-yellow">
+                  <h5 className="text__sm  text-dark-200">New Laptop</h5>
+                  <div className="content-box__data text-dark-100">
+                    <p className="text__sm text__bold">
+                      $1000
+                    </p>
+                  </div>
+              </div>
+            {/* Box with Line at the left side the classname text-... change the color of the line */}
           </div>
         </div>
+        </section>
         {/* Content Box dark */}
+        
+        <section className="section">
+          <div className="content-box">
+            <div className="content-box__header">
+                <h3 className="content-box__title text__md">Transaction</h3>
+                  <Link href="#">
+                    <span className="text text__sm">
+                      See Details
+                    </span>
+                    <span className="icon">
+                      <svg width="5" height="10" viewBox="0 0 5 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M0.765312 0.984686L4.51531 4.73469C4.55018 4.76951 4.57784 4.81087 4.59671 4.8564C4.61558 4.90192 4.62529 4.95072 4.62529 5C4.62529 5.04928 4.61558 5.09808 4.59671 5.1436C4.57784 5.18913 4.55018 5.23048 4.51531 5.26531L0.765313 9.01531C0.712867 9.06782 0.646022 9.10358 0.57324 9.11807C0.500458 9.13257 0.425012 9.12514 0.356454 9.09673C0.287895 9.06833 0.229307 9.02022 0.188105 8.95849C0.146903 8.89677 0.124942 8.82421 0.125 8.75L0.125 1.25C0.124941 1.17579 0.146903 1.10323 0.188105 1.04151C0.229306 0.979783 0.287895 0.931674 0.356453 0.903266C0.425012 0.874858 0.500458 0.867432 0.57324 0.881926C0.646022 0.89642 0.712867 0.932182 0.765312 0.984686Z" fill="#696868"/>
+                      </svg>
+                    </span>
+                  </Link>
+            </div>
+            <div className="content-box content-box__avatar content-box__limit">
+              <Image src={emma} alt="Emma Richardson" width="32" style={{borderRadius:"50%"}}/>
+              <h4 className="avatar__name">
+                Emma Richardson
+              </h4>
+              <div className="data">
+                  <p>
+                    <span className="text-second-green text__bold">
+                      +$75.50
+                    </span> <br />
+                    <small className="text__xs text_light_300">
+                      19 Aug 2024
+                    </small>
+                  </p>
+                  
+              </div>
+            </div>
+          </div>
+        </section>
       </div>
+      {/* container end */}
     </>
   );
 }
